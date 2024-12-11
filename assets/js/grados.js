38c069c2f6f40bc15611eedce8e6813af2446554
gooigi <lopez.gallego2611@gmail.com>
2024-12-11 06:06:36 -0500

    commit

---
+++ assets/js/grados.js
@@ -0,0 +1,5 @@
+let api = "https://interpolice-omfr.onrender.com/api/delitos/";
+let contenido = document.querySelector("#contenido");
+let btnNuevoDelito = document.querySelector("#btnNuevoDelito");
+let frmAction = "";
+let frmDelitos = document.querySelector("#frmDelitos")
